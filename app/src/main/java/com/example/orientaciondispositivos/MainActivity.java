ecfe2fef5e0a8a1b9a3ce21c35e4f6ed3eaff3eb
Isaac <isaacfito07@gmail.com>
2022-10-04 11:53:24 -0600

    Casi listo

--- app/src/main/java/com/example/orientaciondispositivos/MainActivity.java
+++ app/src/main/java/com/example/orientaciondispositivos/MainActivity.java
@@ -2,13 +2,67 @@ package com.example.orientaciondispositivos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private long last_update = 0, last_movement = 0;
+    private float prevX = 0, prevY = 0, prevZ = 0;
+    private float curX = 0, curY = 0, curZ = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        synchronized (this) {
+            long current_time = event.timestamp;
+            curX = event.values[0];
+            curY = event.values[1];
+            curZ = event.values[2];
+
+            if (prevX == 0 && prevY == 0 && prevZ == 0) {
+                last_update = current_time;
+                last_movement = current_time;
+                prevX = curX;
+                prevY = curY;
+                prevZ = curZ;
+            }
+
+            long time_difference = current_time - last_update;
+            if (time_difference > 0) {
+                float movement = Math.abs((curX + curY + curZ) - (prevX - prevY - prevZ)) / time_difference;
+                int limit = 1500;
+                float min_movement = 1E-6f;
+                if (movement > min_movement) {
+                    if (current_time - last_movement >= limit) {
+                        Toast.makeText(getApplicationContext(), "Hay movimiento de " + movement, Toast.LENGTH_SHORT).show();
+                    }
+                    last_movement = current_time;
+                }
+            }
+            prevX = curX;
+            prevY = curY;
+            prevZ = curZ;
+            last_update = current_time;
+        }
+
+        ((TextView) findViewById(R.id.txtAccX)).setText("Acelerómetro X: " + curX);
+        ((TextView) findViewById(R.id.txtAccY)).setText("Acelerómetro Y: " + curY);
+        ((TextView) findViewById(R.id.txtAccZ)).setText("Acelerómetro Z: " + curZ);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
